0c3a796bc4d9850eb4b6c4ea5eb14f8feda03a45
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-03-15 21:18:48 +0530

    adding more string function in the String_function.php file

--- String_function.php
+++ String_function.php
@@ -1,6 +1,14 @@
 <?php
-$name = "mayank";
+$name = "this is my simple string";
 print $name;
 print "<br>";
 echo "The length of " . "my string is : " . strlen($name);
+print "<br>";
+echo str_word_count($name);
+print "<br>";
+echo strrev($name);
+print "<br>";
+echo strpos($name, " is");
+print "<br>";
+echo str_replace("string", "sequence of characters", $name);
 ?>
