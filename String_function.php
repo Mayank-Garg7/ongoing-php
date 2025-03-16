187b7c5271940dc31b967ebf4357866bfdcaad29
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-03-16 23:09:14 +0530

    formating the document with good appealing and easy to manage

--- String_function.php
+++ String_function.php
@@ -1,20 +1,20 @@
 <?php
-$name = "this is my simple string";
-print $name;
-print "<br>";
-echo "The length of " . "my string is : " . strlen($name);
-print "<br>";
-echo str_word_count($name);
-print "<br>";
-echo strrev($name);
-print "<br>";
-echo strpos($name, " is");
-print "<br>";
-echo str_replace("string", "sequence of characters", $name);
-print "<br>";
-echo str_repeat($name, 4);
-print "<br>";
-echo ltrim("    this text is trimmed from left side");
-print "<br>";
-echo rtrim("this text is trimmed from right side    ");
+    $name = "this is my simple string";
+    print $name;
+    print "<br>";
+    echo "The length of " . "my string is : " . strlen($name);
+    print "<br>";
+    echo str_word_count($name);
+    print "<br>";
+    echo strrev($name);
+    print "<br>";
+    echo strpos($name, " is");
+    print "<br>";
+    echo str_replace("string", "sequence of characters", $name);
+    print "<br>";
+    echo str_repeat($name, 4);
+    print "<br>";
+    echo ltrim("    this text is trimmed from left side");
+    print "<br>";
+    echo rtrim("this text is trimmed from right side    ");
 ?>
