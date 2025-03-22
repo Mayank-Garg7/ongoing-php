45c3a3a8f359239a1ef29a60f8bab1e7346604ac
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-03-22 09:03:12 +0530

    defining it's structure in better way for make it more structured and easy to maintain

--- Create.php
+++ Create.php
@@ -1,26 +1,24 @@
 <?php 
-$servernmae = "localhost";
-$username = "root";
-$password = "";
+    $servernmae = "localhost";
+    $username = "root";
+    $password = "";
+    $conn = MySQLi_connect($servernmae, $username, $password);
 
-$conn = MySQLi_connect($servernmae, $username, $password);
-
-if(!$conn){
-    die("sorry we failed to connect: ".mysqli_connect_error());
-}
-else{
-    echo "Connection was successful";
-}
-
-
-$sql = "CREATE DATABASE mayankDb";    
-$result = MySQLi_query($conn, $sql);
-if($result){
-    echo "The db was created successfully";
-}
-else{
-    echo "The db was not created successfully because of this error --> " . mysqli_error($conn);
-}
+    
+    if(!$conn){
+        die("sorry we failed to connect: ".mysqli_connect_error());
+    }
+    else{
+        echo "Connection was successful";
+    }
+    $sql = "CREATE DATABASE mayankDb";    
+    $result = MySQLi_query($conn, $sql);
 
 
+    if($result){
+        echo "The db was created successfully";
+    }
+    else{
+        echo "The db was not created successfully because of this error --> " . mysqli_error($conn);
+    }
 ?>
