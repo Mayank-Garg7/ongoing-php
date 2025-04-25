0b637575822e02ccb5d50c5d9a09a9be1207de6d
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-04-25 21:07:43 +0530

    Add files via upload

---
+++ class_php/array.php
@@ -0,0 +1,51 @@
+<?php
+// $season = array("winter", "summer", "spring", "autunm");
+// echo "seasons are $season[0], $season[1], $season[2], and $season[3]";
+
+// $season[0] = "mayank";
+// $season[1] = "harish";
+// $season[2] = "winter";
+// $season[3] = "sasank";
+// echo "seasons are $season[0], $season[1], $season[2], and $season[3]";
+
+// $season["n"] = "vidyut";
+// $season["d"] = "harish";
+// $season["h"] = "vivan";
+// $season["g"] = "sasank";
+// echo 'friends are '. $season["n"].' '. $season["d"].' '.$season["h"].' and '.$season["g"];
+
+// $season = array("n"=>"shriyansh", "d"=>"abhay");
+// echo 'friends are '. $season["n"].' and '. $season["d"];
+
+
+// $table = array(
+    //     array("s","name","salary"),
+    //     array(1,"sanu", 45000),
+    //     array(2,"john", 35000),
+//     array(3,"tanu", 40000)
+// );
+// for ($i=0; $i < sizeof($table); $i++) { 
+    //     for ($j=0; $j < 3; $j++) { 
+//         echo $table[$i][$j]." | ";
+//     }
+//     echo "<br>";
+// }
+
+
+$arr = array(1,5,2,6,8,9);
+$b = 1;
+// rsort($arr, 0);
+// print_r($arr);
+// sort($arr);
+// print_r($arr);
+arsort($arr);
+print_r($arr);
+
+
+
+// $season = array("n"=>"shriyansh", "d"=>"abhay");
+// ksort($season);
+// print_r($season);
+
+let's see this don't make noise here please 
+?>
