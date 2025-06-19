edd5d4679aad4ab99935a3fc6c6c1ae0992d006f
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-06-19 14:49:04 +0530

    Update multidimensionArray.php

--- multidimensionArray.php
+++ multidimensionArray.php
@@ -1,49 +1,48 @@
 <?php
+// Define a 4-dimensional array named $favColor
 $favColor = array(
-                array(
-                    array(
-                        array(1,2,3),
-                        array(4,5,6)
-                    ),
-                    array(
-                        array(7,8,9),
-                        array(1,5,7)
-                    ),
-                ),
-                array(
-                    array(
-                        array(7,8,9),
-                        array(1,5,7)
-                    ),
-                    array(
-                        array(1,2,3),
-                        array(4,5,6)
-                    ),
-                )
-            );
-// foreach ($favColor as $a) {
-//     foreach ($a as $b) {
-//         foreach ($b as $c) {
-//             foreach ($c as $d) {
-//                echo $d." ";
-//             }
-//             echo "<br>";
-//         }
-//         echo "<br><br>";
-//     }
-//     echo "<br><br><br>";
-// }
+    array( // First layer (0th index)
+        array( // Second layer (0th index of 0th)
+            array(1, 2, 3),     // Third layer (0th index of 0th of 0th)
+            array(4, 5, 6)      // Third layer (1st index of 0th of 0th)
+        ),
+        array( // Second layer (1st index of 0th)
+            array(7, 8, 9),
+            array(1, 5, 7)
+        )
+    ),
+    array( // First layer (1st index)
+        array(
+            array(7, 8, 9),
+            array(1, 5, 7)
+        ),
+        array(
+            array(1, 2, 3),
+            array(4, 5, 6)
+        )
+    )
+);
 
-for ($i=0; $i < count($favColor); $i++) { 
-    for ($j=0; $j < count($favColor[$i]); $j++) { 
-        for ($k=0; $k < count($favColor[$i][$j]); $k++) { 
-            for ($l=0; $l < count($favColor[$i][$j][$k]); $l++) { 
-                echo $favColor[$i][$j][$k][$l]." ";
+// Using nested for loops to traverse and print the values of the 4D array
+
+for ($i = 0; $i < count($favColor); $i++) { // Loop through the 1st dimension
+    for ($j = 0; $j < count($favColor[$i]); $j++) { // Loop through the 2nd dimension
+        for ($k = 0; $k < count($favColor[$i][$j]); $k++) { // Loop through the 3rd dimension
+            for ($l = 0; $l < count($favColor[$i][$j][$k]); $l++) { // Loop through the 4th dimension (actual values)
+                
+                // Print each element of the innermost array followed by a space
+                echo $favColor[$i][$j][$k][$l] . " ";
             }
+
+            // Line break after each innermost array
             echo "<br>";
         }
+
+        // Extra line break after finishing one 3rd-level array
         echo "<br><br>";
     }
+
+    // Extra line breaks after finishing one 2nd-level array
     echo "<br><br><br>";
 }
 ?>
