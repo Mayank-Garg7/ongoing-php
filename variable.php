61d3775527df1527b4b27b525c5713379a557b05
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-06-19 14:50:06 +0530

    Update variable.php

--- variable.php
+++ variable.php
@@ -1,8 +1,17 @@
 <?php
+        // Initialize variable $a with value 4
         $a = 4;
+
+        // Initialize variable $b with value 8
         $b = 8;
-        $c = $a+$b;
+
+        // Add $a and $b and store the result in variable $c
+        $c = $a + $b;
+
+        // Print the sum using the print statement
         print "The sum of $a and $b is : $c<br>";
 
+        // Use var_dump to display the data type and value of $a
+        // It will output: int(4)
         echo var_dump($a);
 ?>
