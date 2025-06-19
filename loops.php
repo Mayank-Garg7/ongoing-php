407c50df24c8cf2216e1e44c52a1f57d925e891c
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-06-19 14:47:22 +0530

    Update loops.php

--- loops.php
+++ loops.php
@@ -1,8 +1,15 @@
 <?php
-$x = 1;
-while($x!=5){
-    $x = rand(1,10);
-    echo "The value of x is $x<br>";
-}
+    // Initialize variable $x with the value 1
+    $x = 1;
 
+    // Start a while loop that runs as long as $x is not equal to 5
+    while($x != 5){
+        // Generate a random number between 1 and 10 and assign it to $x
+        $x = rand(1, 10);
+
+        // Print the current value of $x followed by a line break
+        echo "The value of x is $x<br>";
+    }
+
+    // The loop stops when $x becomes equal to 5
 ?>
