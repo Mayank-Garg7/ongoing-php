0f11972726e3d5251d1aeb4cedc7138cf58efdb0
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-06-25 18:20:42 +0530

    Update handlePost.php

--- class_php/get_and_postMethods/byUsing_seperate_files/handlePost.php
+++ class_php/get_and_postMethods/byUsing_seperate_files/handlePost.php
@@ -1,16 +1,20 @@
 <?php 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+// Check if the request method is POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve 'user_name' and 'password' from the POST request
     $name = $_POST['user_name'];
     $pass = $_POST['password'];
 }
-if(empty($name)){
+
+// Check if the 'name' field is empty
+if (empty($name)) {
+    // If the name is empty, show a message
     echo "Name is empty";
-}
-else{
-    echo $name;
-    echo "<br>" . $pass;
+} else {
+    // If the name is not empty, display the name and password
+    echo $name;              // Display the entered user name
+    echo "<br>" . $pass;     // Display the entered password on the next line
 }
 
 ?>
-
-
