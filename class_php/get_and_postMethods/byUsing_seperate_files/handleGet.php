7156ba7081e53579f3d8e6f0c8b2de4a32ddbce1
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2025-06-25 18:19:47 +0530

    Update handleGet.php

--- class_php/get_and_postMethods/byUsing_seperate_files/handleGet.php
+++ class_php/get_and_postMethods/byUsing_seperate_files/handleGet.php
@@ -1,13 +1,18 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){
+// Check if the request method is GET
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Retrieve 'user_name' and 'password' from the GET request
     $name = $_GET['user_name'];
     $pass = $_GET['password'];
 }
-if(empty($name)){
+
+// Check if the 'name' variable is empty
+if (empty($name)) {
+    // If name is empty, display a message
     echo "Name is empty";
-}
-else{
+} else {
+    // If name is not empty, display the name and password
     echo $name;
     echo "<br>" . $pass;
 }
